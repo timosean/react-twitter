a87e15b5c318da86857a9cfa6ae22715adda2aa8
Seungwoo <timosean@naver.com>
2024-04-14 17:52:04 +0900

    [feat] add Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import Router from "components/Router";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<h1>Home Page</h1>} />
-            <Route path="/posts" element={<h1>Post List Page</h1>} />
-            <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-            <Route path="/profile" element={<h1>Profile</h1>} />
-        </Routes>
-    );
+    return <Router />;
 }
 
 export default App;
---
+++ src/components/Router.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "pages/home";
+
+export default function Router() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/posts" element={<h1>Post List Page</h1>} />
+            <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+            <Route path="/posts/new" element={<h1>New Post Page</h1>} />
+            <Route path="/profile" element={<h1>Profile</h1>} />
+            <Route path="/profile/edit" element={<h1>Edit Profile Page</h1>} />
+            <Route path="/notifications" element={<h1>Notifications Page</h1>} />
+            <Route path="/search" element={<h1>Search Page</h1>} />
+            <Route path="/users/login" element={<h1>Login Page</h1>} />
+            <Route path="/users/signup" element={<h1>Signup Page</h1>} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+    );
+}
