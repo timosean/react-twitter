7041b66fd3b4fb3f5b8b93d19a3555e015a7a402
Seungwoo <timosean@naver.com>
2024-04-14 17:28:03 +0900

    [feat] install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/posts" element={<h1>Post List Page</h1>} />
+            <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+            <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+    );
 }
 
 export default App;
